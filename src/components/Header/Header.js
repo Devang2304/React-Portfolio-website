c3b0d2cd5e4455ed0f8962cc63c418dda74d45be
Devang2304 <devangvartak2304@gmail.com>
2022-09-27 01:09:32 +0530

    Created Nav section and added social media icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,46 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di'; //importing icons
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles'; //these are the components that i have used in style
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons,Span } from './HeaderStyles'; //these are the components that i have used in style
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+      <a style={{display:"flex", alignItems:"Center" ,color:'white',marginBottom:'20px'}}>
+        <DiCssdeck size="3rem"/><Span>Portfolio</Span>
+      </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://instagram.com">
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
