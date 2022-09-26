e22e39425b825e6837cf1a1b74fa091a0f34013c
Devang2304 <devangvartak2304@gmail.com>
2022-09-27 00:13:04 +0530

    Created routes and styling components and overall idea for my project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'; //this will allow us to link different pages
+import React from 'react';
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { DiCssdeck } from 'react-icons/di'; //importing icons
+
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles'; //these are the components that i have used in style
+
+const Header = () =>  (
+  <div>
+    Header
+  </div>
+);
+
+export default Header;
