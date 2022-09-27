0661e6e19cb32df2ad5d25caa5e7a131223e49d4
Devang2304 <devangvartak2304@gmail.com>
2022-09-28 01:46:27 +0530

    Added hero section and created project section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents'; //importing global components which  have been created for everywhere to use
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+      Welcome To <br/>
+      My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      I am a Front End Developer with experience building web based projects. One of my biggest strengths is the development and delivering big ideas that amaze people's minds.
+      </SectionText>
+      <Button onClick={()=>window.location='https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
